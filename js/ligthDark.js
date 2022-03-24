08ddbd3fdea9cd549d865ba9b6d9ebc669459cae
Gustavo Andrade <gandrade@digitalhub.com.br>
2022-03-24 00:29:14 -0300

    Add function for change images in darkLigh mode.

--- js/ligthDark.js
+++ js/ligthDark.js
@@ -1,6 +1,16 @@
 var content = document.getElementsByTagName('body')[0];
 var darkMode = document.getElementById('dark-change');
+var logoMain = document.querySelector("#logo-main");
+var logoSignIn = document.querySelector("#icon-sign");
+
 darkMode.addEventListener('click', function(){
     darkMode.classList.toggle('active');
     content.classList.toggle('dark');
-})
+    if (logoMain.src.includes("light")) {
+        logoMain.src = "/images/apple-music-logo-dark.png";
+        logoSignIn.src = "/images/sign-in-light.png";
+    } else {
+        logoMain.src = "/images/apple-music-logo-light.png";
+        logoSignIn.src = "/images/sign-in-dark.png";
+      }
+}) 
